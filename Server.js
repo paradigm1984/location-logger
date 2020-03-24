3945b24f4833e609b40bab904a14ec6559a211ca
Daniel Blanco <dan@danblanco.com>
2020-03-23 22:12:10 -0400

    fixing deployment bug

--- Server.js
+++ Server.js
@@ -4,12 +4,12 @@ const path = require("path");
 const port = process.env.PORT || 5000;
 
 //Static file
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 //production mode
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client/build')));
-	app.get('*', (req, res) => { res.sendfile(path.join(__dirname = 'client/build/index.html')); });
+	app.use(express.static(path.join(__dirname, '/public/')));
+	app.get('*', (req, res) => { res.sendfile(path.join(__dirname = '/public/index.html')); });
 }
 
 app.get('*', (req, res) => { res.sendFile(path.join(__dirname + '/public/index.html')); });
