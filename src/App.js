ae72fba31213baf32c318e677261be9c6a0bf8eb
Daniel Blanco <dan@danblanco.com>
2020-03-23 20:43:27 -0400

    fixing issues to deploy to heroku

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    Location Logger
+    <div>
+      <Header />
+      <Balance />
     </div>
   );
 }
--- src/Server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-onst cors = require("cors");
-const mongoose = require("mongoose");
-const logger = require("morgan");
