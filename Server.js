d6c99d328e99586481d07dd7d41c89c9a9710f99
Daniel Blanco <dan@danblanco.com>
2020-03-23 22:08:18 -0400

    fixing deployment bug

--- Server.js
+++ Server.js
@@ -9,10 +9,11 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 //production mode
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'client/build')));
-	//  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
-	//build mode
+	app.get('*', (req, res) => { res.sendfile(path.join(__dirname = 'client/build/index.html')); });
 }
-	app.get('*', (req, res) => { res.sendFile(path.join(__dirname + '/client/public/index.html')); });
+
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname + '/public/index.html')); });
+	
 	//start server
 	app.listen(port, (req, res) => { console.log(`server listening on port: ${port}`); });
 
