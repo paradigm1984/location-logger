1a873e9541b1f6f6957c4bfbc3e920ae52a83a2a
Daniel Blanco <dan@danblanco.com>
2020-03-23 21:43:14 -0400

    fixing deployment bug

--- Server.js
+++ Server.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	//  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
 	//build mode
+}
 	app.get('*', (req, res) => { res.sendFile(path.join(__dirname + '/client/public/index.html')); });
 	//start server
 	app.listen(port, (req, res) => { console.log(`server listening on port: ${port}`); });
