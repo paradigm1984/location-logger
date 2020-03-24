ff4f74c26bbd013001db5f0fd7cebbde3dfb63c6
Daniel Blanco <dan@danblanco.com>
2020-03-23 23:31:10 -0400

    configuring app for heroku

---
+++ build/precache-manifest.d86ecd8799847d9e3e29b2818bfae8b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f54c0fe41ea327399ae3b443297030d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23982b3591eb0eaa9593",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b11e5f1054e7b3e6ceb9",
+    "url": "/static/js/2.b95f9d3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b95f9d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23982b3591eb0eaa9593",
+    "url": "/static/js/main.8e373af9.chunk.js"
+  },
+  {
+    "revision": "e1cc3bcc1883fdf3eb24",
+    "url": "/static/js/runtime-main.79db55a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
